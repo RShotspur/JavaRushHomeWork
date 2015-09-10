8027a0c4a1cba0ffe645d8a3faced07c90b5fc6f
RS <rs.hotspur@gmail.com>
2015-09-10 16:12:48 +0300

    New Lesson Update

---
+++ src/com/javarush/test/level13/lesson11/home03/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.test.level13.lesson11.home03;
+
+/* Чтение файла
+1. Считать с консоли имя файла.
+2. Вывести в консоль(на экран) содержимое файла.
+3. Не забыть освободить ресурсы. Закрыть поток чтения с файла и поток ввода с клавиатуры.
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        //add your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        FileInputStream readFromFile = new FileInputStream(fileName);
+        while (readFromFile.available() > 0)
+        {
+            System.out.print((char)readFromFile.read());
+        }
+        readFromFile.close();
+        reader.close();
+    }
+}
