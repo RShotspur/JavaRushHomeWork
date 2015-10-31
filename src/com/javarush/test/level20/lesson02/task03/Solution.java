9161edb74d233e7908b61f11a570e838ca5b45b3
RS <rs.hotspur@gmail.com>
2015-10-31 12:48:22 +0200

    New Lesson Update

--- src/com/javarush/test/level20/lesson02/task03/Solution.java
+++ src/com/javarush/test/level20/lesson02/task03/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.test.level20.lesson02.task03;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /* Знакомство с properties
 В методе fillInPropertiesMap считайте имя файла с консоли и заполните карту properties данными из файла.
@@ -12,15 +13,32 @@ import java.util.Map;
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
+    public void fillInPropertiesMap() throws Exception
+    {
         //implement this method - реализуйте этот метод
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream inputStream = new FileInputStream(reader.readLine());
+        load(inputStream);
+        inputStream.close();
     }
 
     public void save(OutputStream outputStream) throws Exception {
         //implement this method - реализуйте этот метод
+        Properties property = new Properties();
+        for (Map.Entry<String, String> pair : properties.entrySet())
+            property.put(pair.getKey(), pair.getValue());
+
+        property.store(outputStream, "");
+
     }
 
     public void load(InputStream inputStream) throws Exception {
         //implement this method - реализуйте этот метод
+        Properties property = new Properties();
+        property.load(inputStream);
+
+        for (Map.Entry<Object, Object> pair : property.entrySet())
+            properties.put(String.valueOf(pair.getKey()), String.valueOf(pair.getValue()));
+
     }
 }
